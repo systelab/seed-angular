5b6dc82261c145b07be2a4d4a521bfd001c6dd75
alfonsserra <alfonsoserra@gmail.com>
2018-01-19 18:35:04 +0100

    Use last systelab libraries.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
 
 @Component({
 	selector: 'app-root',
 	template: `
-                <router-outlet></router-outlet>`
+                <router-outlet></router-outlet>`,
+	styleUrls: ['app.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
 	constructor(protected i18nService: I18nService) {
