fa534663e70e5d33c3f0bd4ace01e825e1962abb
alfonsserra <alfonsoserra@gmail.com>
2018-01-23 09:30:16 +0100

    Add error message and use standard forms.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiGlobalsService } from '../globals/globals.service';
 import { UserService } from '../common/api/user.service';
+import { MessagePopupService } from 'systelab-components/widgets/modal';
 
 @Component({
 	selector:    'login',
@@ -12,17 +13,23 @@ export class LoginComponent {
 	public userName = '';
 	public password = '';
 
-	constructor(protected router: Router, protected userService: UserService, protected apiGlobalsService: ApiGlobalsService) {
+	constructor(protected router: Router, protected userService: UserService, protected apiGlobalsService: ApiGlobalsService,
+	            protected messagePopupService: MessagePopupService) {
 
 	}
 
 	public doLogin() {
-		this.userService.authenticateUser(this.userName, this.password)
-			.subscribe(
-				(response) => {
-					this.apiGlobalsService.bearer = response.headers.get('Authorization');
-					this.router.navigateByUrl('/main');
-				}
-			);
+		if (this.userName && this.password) {
+			this.userService.authenticateUser(this.userName, this.password)
+				.subscribe(
+					(response) => {
+						this.apiGlobalsService.bearer = response.headers.get('Authorization');
+						this.router.navigateByUrl('/main');
+					},
+					() => {
+						this.messagePopupService.showErrorPopup('Credentials', 'Invalid username or password');
+					}
+				);
+		}
 	}
 }
