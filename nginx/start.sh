a2c86ace4fd7d74a8607dda25cb668b770f9116a
alfonsserra <alfonsoserra@gmail.com>
2018-02-11 13:58:50 +0100

    Remove quotes from the backend

--- nginx/start.sh
+++ nginx/start.sh
@@ -4,7 +4,7 @@ if [ -z ${BACKEND+x} ]; then
     echo "BACKEND is unset. Default value will be used.";
 else
     echo "BACKEND is set to '$BACKEND'.";
-    sed -i -e "s~http://127.0.0.1:8080~'$BACKEND'~g" /usr/share/nginx/html/main.*.bundle.js
+    sed -i -e "s~http://127.0.0.1:8080~$BACKEND~g" /usr/share/nginx/html/main.*.bundle.js
 fi
 
 nginx -g "daemon off;"
