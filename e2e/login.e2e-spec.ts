67d2647942864303cdfb8418b9f582d52b9178b0
olgapuig <olgapuig@gmail.com>
2018-04-27 16:00:24 +0200

    Fix login test case according to the best practices on test design (#3)
    * Fix login test case according to the best practices on test design
    
    * Fix login test case according to the best practices on test design

--- e2e/login.e2e-spec.ts
+++ e2e/login.e2e-spec.ts
@@ -8,14 +8,18 @@ describe('Seed Angular: Login', () => {
 	let main: MainPage;
 
 	function navigateToHomePage() {
-		allure.createStep('Opening main page', function(){})();
-		login.navigateToHomePage();
+		allure.createStep('Opening main page', () => {
+			login.navigateToHomePage();
+		})();
 	}
 
 	function setUserNameAndPassword(userName: string, password: string) {
-		allure.createStep(`Set username as '${userName}' and password as '${password}'`, function(){})();
-		login.getUsernameField().sendKeys(userName);
-		login.getPasswordField().sendKeys(password);
+		allure.createStep(`Set username as '${userName}' and password as '${password}'`, () => {
+			login.getUsernameField()
+				.sendKeys(userName);
+			login.getPasswordField()
+				.sendKeys(password);
+		})();
 	}
 
 	beforeEach(() => {
@@ -25,26 +29,38 @@ describe('Seed Angular: Login', () => {
 		main = new MainPage();
 	});
 
-	it('Should display an error message if password is not correct', () => {
+	it('Shall display an error message if password is not correct', () => {
 		allure.severity(allure.SEVERITY.BLOCKER);
 		allure.story('Login');
 		navigateToHomePage();
 		setUserNameAndPassword('Systelab', 'wrong password');
-		allure.createStep('Click on Enter button', function(){})();
-		login.getEnterButton().click();
-		allure.createStep('Check that I have an error', function(){})();
-		expect(login.getErrorMessage().getText()).toBe('Invalid username or password');
+		allure.createStep('Perform Login', () => {
+			login.getEnterButton()
+				.click();
+			allure.createStep('The application returns an error', () => {
+				expect(login.getErrorMessage()
+					.getText())
+					.toBe('Invalid username or password');
+			})();
+		})();
+
 	});
 
-	it('Should enter if password is correct', () => {
-		allure.description('Should enter if password is correct');
+	it('Shall enter if password is correct', () => {
+		allure.description('Shall enter if password is correct');
 		allure.severity(allure.SEVERITY.BLOCKER);
 		allure.story('Login');
 		navigateToHomePage();
 		setUserNameAndPassword('Systelab', 'Systelab');
-		allure.createStep('Click on Enter button', function(){})();
-		login.getEnterButton().click();
-		allure.createStep('Check that I see the main page', function(){})();
-		expect(main.getFullUsernameField().getText()).toBe('Administrator');
+		allure.createStep('Perform Login', () => {
+			login.getEnterButton()
+				.click();
+			allure.createStep('Check that I see the main page', () => {
+				expect(main.getFullUsernameField()
+					.getText())
+					.toBe('Administrator');
+			})();
+		})();
+
 	});
 });
