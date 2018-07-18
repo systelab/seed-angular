b5d14402ad4abe6006a2a8b9de93069268224cc3
alfonsserra <alfonsoserra@gmail.com>
2018-07-18 20:55:16 +0200

    Simplify language array

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,4 @@
-
-import { of as observableOf, Observable } from 'rxjs';
+import { Observable, of as observableOf } from 'rxjs';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
 import { ApiGlobalsService } from '../globals/globals.service';
@@ -13,7 +12,7 @@ import { ChangePasswordDialog, ChangePasswordDialogParameters } from 'systelab-l
 import { LocalStorageService } from 'systelab-preferences/lib/local-storage.service';
 
 @Component({
-	selector: 'main',
+	selector:    'main',
 	templateUrl: 'main.component.html'
 })
 export class MainComponent implements OnInit {
@@ -33,21 +32,20 @@ export class MainComponent implements OnInit {
 	public comboOptionList: Array<Object> = [];
 
 	constructor(private router: Router, protected messagePopupService: MessagePopupService,
-		protected dialogService: DialogService, protected i18nService: I18nService,
-		protected apiGlobalsService: ApiGlobalsService, private localStorage: LocalStorageService) {
+	            protected dialogService: DialogService, protected i18nService: I18nService,
+	            protected apiGlobalsService: ApiGlobalsService, private localStorage: LocalStorageService) {
 		this.frameWidth = (window.innerWidth);
 		this.frameHeight = (window.innerHeight);
 		this.comboOptionList = [
-			{ description: 'EN', id: 1 },
-			{ description: 'ES', id: 2 },
-			{ description: 'IT', id: 3 },
-			{ description: 'KR', id: 4 }
+			{description: 'EN', id: 1},
+			{description: 'ES', id: 2},
+			{description: 'IT', id: 3},
+			{description: 'KR', id: 4}
 		];
 		const language = localStorage.get('language');
 		if (!language) {
 			this.desLanguage = 'EN';
-		}
-		else {
+		} else {
 			const lang = JSON.parse(language);
 			this.desLanguage = lang.value.description;
 		}
@@ -67,37 +65,40 @@ export class MainComponent implements OnInit {
 	public setMenu() {
 
 		this.menu = [];
-		this.i18nService.get(['COMMON_SETUP', 'COMMON_CHANGE_PASSWORD', 'COMMON_CHANGE_USER', 'COMMON_ABOUT', 'COMMON_TAB_ONE', 'COMMON_TAB_TWO', 'COMMON_TAB_THREE', 'COMMON_TAB_FOUR']).subscribe((res) => {
-			if (this.frameWidth < 1024) {
-				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_ONE, false, () => this.doSelect('T1')));
-				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_TWO, false, () => this.doSelect('T2')));
-				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_THREE, false, () => this.doSelect('T3')));
-				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_FOUR, false, () => this.doSelect('T4')));
-				this.menu.push(new ApplicationHeaderMenuEntry('', true));
-			}
+		this.i18nService.get(['COMMON_SETUP', 'COMMON_CHANGE_PASSWORD', 'COMMON_CHANGE_USER', 'COMMON_ABOUT', 'COMMON_TAB_ONE', 'COMMON_TAB_TWO', 'COMMON_TAB_THREE', 'COMMON_TAB_FOUR'])
+			.subscribe((res) => {
+				if (this.frameWidth < 1024) {
+					this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_ONE, false, () => this.doSelect('T1')));
+					this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_TWO, false, () => this.doSelect('T2')));
+					this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_THREE, false, () => this.doSelect('T3')));
+					this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_TAB_FOUR, false, () => this.doSelect('T4')));
+					this.menu.push(new ApplicationHeaderMenuEntry('', true));
+				}
 
-			this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_SETUP, false, () => this.doShowSettings()));
-			this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_CHANGE_PASSWORD, false, () => this.doChangePassword()));
-			this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_CHANGE_USER, false, () => this.doLogout()));
-			this.menu.push(new ApplicationHeaderMenuEntry('', true));
-			this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_ABOUT, false, () => this.doShowAbout()));
-		});
+				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_SETUP, false, () => this.doShowSettings()));
+				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_CHANGE_PASSWORD, false, () => this.doChangePassword()));
+				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_CHANGE_USER, false, () => this.doLogout()));
+				this.menu.push(new ApplicationHeaderMenuEntry('', true));
+				this.menu.push(new ApplicationHeaderMenuEntry(res.COMMON_ABOUT, false, () => this.doShowAbout()));
+			});
 
 	}
 
 	public setSideTabs() {
-		this.i18nService.get(['COMMON_TAB_ONE', 'COMMON_TAB_TWO', 'COMMON_TAB_THREE', 'COMMON_TAB_FOUR']).subscribe((res) => {
-			this.sidetabs.push(new ApplicationSidebarTab('T1', res.COMMON_TAB_ONE, true));
-			this.sidetabs.push(new ApplicationSidebarTab('T2', res.COMMON_TAB_TWO, false));
-			this.sidetabs.push(new ApplicationSidebarTab('T3', res.COMMON_TAB_THREE, false));
-			this.sidetabs.push(new ApplicationSidebarTab('T4', res.COMMON_TAB_FOUR, false));
-		});
+		this.i18nService.get(['COMMON_TAB_ONE', 'COMMON_TAB_TWO', 'COMMON_TAB_THREE', 'COMMON_TAB_FOUR'])
+			.subscribe((res) => {
+				this.sidetabs.push(new ApplicationSidebarTab('T1', res.COMMON_TAB_ONE, true));
+				this.sidetabs.push(new ApplicationSidebarTab('T2', res.COMMON_TAB_TWO, false));
+				this.sidetabs.push(new ApplicationSidebarTab('T3', res.COMMON_TAB_THREE, false));
+				this.sidetabs.push(new ApplicationSidebarTab('T4', res.COMMON_TAB_FOUR, false));
+			});
 	}
 
 	public setSideButtons() {
-		this.i18nService.get('COMMON_DOCUMENTATION').subscribe((res) => {
-			this.sideactions.push(new ApplicationSidebarAction(res, () => this.doShowDocumentation()));
-		});
+		this.i18nService.get('COMMON_DOCUMENTATION')
+			.subscribe((res) => {
+				this.sideactions.push(new ApplicationSidebarAction(res, () => this.doShowDocumentation()));
+			});
 	}
 
 	@HostListener('window:resize', ['$event'])
@@ -141,10 +142,11 @@ export class MainComponent implements OnInit {
 
 	public changePassword(oldPassword: string, newPassword: string): Observable<boolean> {
 		if (oldPassword === newPassword) {
-			this.i18nService.get(['ERR_ERROR', 'ERR_IMPOSSIBLE_CHANGE_PASSWORD']).subscribe((res) => {
-				this.messagePopupService.showErrorPopup(res.COMMON_ERROR, res.COMMON_IMPOSSIBLE_CHANGE_PASSWORD);
-				return observableOf(false);
-			});
+			this.i18nService.get(['ERR_ERROR', 'ERR_IMPOSSIBLE_CHANGE_PASSWORD'])
+				.subscribe((res) => {
+					this.messagePopupService.showErrorPopup(res.COMMON_ERROR, res.COMMON_IMPOSSIBLE_CHANGE_PASSWORD);
+					return observableOf(false);
+				});
 
 		}
 		return observableOf(true);
@@ -153,19 +155,12 @@ export class MainComponent implements OnInit {
 	public comboChangeEvent(e: any): void {
 
 		let language = 'en';
-		if (e.id === 1) {
-			language = 'en';
+		if (e.id > 0 && e.id < 5) {
+			const languages: string[] = ['en', 'es-ES', 'it-IT', 'kr-KR'];
+			language = languages[e.id - 1];
 		}
-		else if (e.id === 2) {
-			language = 'es-ES';
-		}
-		else if (e.id === 3) {
-			language = 'it-IT';
-		}
-		else if (e.id === 4) {
-			language = 'kr-KR';
-		}
-		this.localStorage.put('language', JSON.stringify({ value: e }));
+
+		this.localStorage.put('language', JSON.stringify({value: e}));
 		this.i18nService.use(language)
 			.subscribe(
 				() => window.location.reload(),
