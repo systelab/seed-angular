af5fdf21447169a6dabb66ef280cc0e703ad6391
alfonsserra <alfonsoserra@gmail.com>
2018-09-18 12:31:12 +0200

    Simplify languages behaviour.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { I18nService } from 'systelab-translate/lib/i18n.service';
 import { Router } from '@angular/router';
-import { ApiGlobalsService } from './globals/globals.service';
 import { LocalStorageService } from 'systelab-preferences/lib/local-storage.service';
 
 @Component({
@@ -13,26 +12,15 @@ import { LocalStorageService } from 'systelab-preferences/lib/local-storage.serv
 })
 export class AppComponent {
 	constructor(protected i18nService: I18nService, private router: Router, private localStorage: LocalStorageService) {
-		const language = localStorage.get('language');
-		let lang = 'en'
-		if (language) {
-			const langJson = JSON.parse(language);
-			if (langJson.value.id === 1) {
-				lang = 'en';
-			}
-			else if (langJson.value.id === 2) {
-				lang = 'es-ES';
-			}
-			else if (langJson.value.id === 3) {
-				lang = 'it-IT';
-			}
-			else if (langJson.value.id === 4) {
-				lang = 'kr-KR';
-			}
-		}
-		i18nService.use(lang)
-			.subscribe(() => console.log('Language set to ' + language),
-				(error) => console.log('Error setting the language.'));
+		try {
+			const previousLanguage = localStorage.get('language');
+			const lang = previousLanguage ? previousLanguage : 'en';
+			i18nService.use(lang)
+				.subscribe(() => console.log('Language set to ' + lang),
+					(error) => console.log('Error setting the language.'));
 
+		} catch (e) {
+			console.log(e);
+		}
 	}
 }
