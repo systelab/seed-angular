dacf47ada76f15a0d91c51f5700ec9bd42e8a2ec
alfonsserra <alfonsoserra@gmail.com>
2019-01-16 12:30:12 +0100

    remove primeng import

--- src/app/common/components/components.module.ts
+++ src/app/common/components/components.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientGrid } from './patient/grid/patient-grid.component';
 import { FormsModule } from '@angular/forms';
-import { ContextMenuModule } from 'primeng/components/contextmenu/contextmenu';
 import { SystelabComponentsModule } from 'systelab-components';
 
 @NgModule({
 	declarations: [PatientGrid],
 	imports:      [CommonModule,
 		FormsModule,
-		SystelabComponentsModule,
-		ContextMenuModule],
+		SystelabComponentsModule],
 	exports:      [PatientGrid]
 })
 export class ComponentsModule {
