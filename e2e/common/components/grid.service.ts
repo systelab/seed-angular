17b55cca1ad40ad2435e70de4c9d412e2998ae53
alfonsserra <alfonsoserra@gmail.com>
2019-11-25 18:40:08 +0100

    Restore the grid service

--- e2e/common/components/grid.service.ts
+++ e2e/common/components/grid.service.ts
@@ -1,12 +1,26 @@
-import { by, ElementFinder } from 'protractor';
+import { by, ElementArrayFinder, ElementFinder } from 'protractor';
 import { ContextMenuService } from './context-menu.service';
 import { ExpectsUtil } from '../utilities/expects-util';
+import { BasePage } from './base-page';
+import { TestUtil } from '../utilities/test-util';
 
 declare const allure: any;
 
 export class GridService {
 	public static readonly GRID_COLUMN_CONTEXT_MENU = 'contextMenu';
 	public static readonly GRID_COLUMN_NAME = 'name';
+	public static readonly GRID_DEFAULT_TAG = 'ag-grid-angular';
+	public static readonly GRID_DEFAULT_ID = 'agGrid';
+	public static readonly GRID_NODATA_MESSAGE = 'No Rows To Show';
+
+	public static getGridComponentById(page: BasePage) {
+		return page.getObjectById(this.GRID_DEFAULT_ID);
+	}
+
+	public static getGridComponent(myElement: ElementFinder) {
+		return myElement
+			.element(by.tagName(this.GRID_DEFAULT_TAG));
+	}
 
 	public static getGridInnerComponent(grid: ElementFinder, col?: string, row?: number): any {
 		if (col === undefined) {
@@ -63,6 +77,54 @@ export class GridService {
 	public static getRow(grid: ElementFinder, row: number) {
 		return grid.all(by.css('div[row-index="' + row + '"] div.ag-cell-value'));
 
+	}
+
+	public static getCol(grid: ElementFinder, col: number) {
+		return grid.all(by.css('div[col-index="' + col + '"] div.ag-cell-value'));
+	}
+
+	public static getGridHeaderText(grid: ElementFinder, col: number) {
+		return GridService.getGridHeader(grid, col)
+			.element(by.className('ag-header-cell-text'))
+			.getText();
+	}
+
+	public static getNoDataMessage(grid: ElementFinder) {
+		return grid.element(by.className('ag-overlay-no-rows-center'));
+	}
+
+	public static getNumberOfRows(grid: ElementFinder) {
+		return grid.all(by.css('div.ag-body-container div.ag-row'))
+			.count();
+	}
+
+	public static gridHasData(grid: ElementFinder): Promise<boolean> {
+		return new Promise((resolve, reject) => {
+			this.getNumberOfRows(grid)
+				.then((num) => {
+					resolve(num > 0);
+				}, (err) => {
+					reject(false);
+				})
+				.catch((err) => {
+					reject(false);
+				});
+		});
+	}
+
+	public static checkGridHeaders(grid: ElementFinder, titles: string[]) {
+		titles.forEach((titleElem, index) => {
+			TestUtil.checkText(GridService.getGridHeaderText(grid, index), `Column[${index}]: ${titleElem}`, titleElem);
+		});
+		GridService.gridHasData(grid)
+			.then((data) => {
+				if (data === true) {
+					TestUtil.checkIsPresent(GridService.getRow(grid, 0)
+						.get(0), 'a Cell');
+				} else {
+					TestUtil.checkText(GridService.getNoDataMessage(grid), 'NoData_Message', GridService.GRID_NODATA_MESSAGE);
+				}
+			});
 	}
 }
 
