7099284bc4f0c22b8a8af05b57f4064a58cd1bd8
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2021-11-08 15:14:47 +0100

    Added spec reporter

--- e2e-wdio/wdio.conf.js
+++ e2e-wdio/wdio.conf.js
@@ -153,6 +153,7 @@ exports.config = {
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
     reporters: [
+        'spec',
         ['allure', {
             outputDir: 'allure-results',
             disableWebdriverStepsReporting: true,
