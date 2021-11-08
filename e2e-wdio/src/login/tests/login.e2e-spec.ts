f5250c48b0edbd5e287b17697bf5fc5111a14fa7
Joaquim Vila <36737101+joaquimvila@users.noreply.github.com>
2021-11-08 15:05:13 +0100

    Commented all allure usage to allow test to be executed successfully (while wrapper is not created)

--- e2e-wdio/src/login/tests/login.e2e-spec.ts
+++ e2e-wdio/src/login/tests/login.e2e-spec.ts
@@ -1,10 +1,10 @@
 import { LoginPage} from '../page-objects/login.po';
 import { MainPage } from '../../main/page-objects/main.po';
-import { because, TestUtil } from 'systelab-components-test/lib/utilities';
+//import { because, TestUtil } from 'systelab-components-test/lib/utilities';
 import { GeneralParameters } from '../../../general-parameters';
 import { LoginActionService } from '../services/login-action.service';
 
-declare const allure: any;
+//declare const allure: any;
 
 describe('TC0002_LoginManagement_e2e', () => {
 	let loginPage: LoginPage;
@@ -23,20 +23,20 @@ describe('TC0002_LoginManagement_e2e', () => {
 	});
 
 	it('Login correct', async () => {
-		await allure.createStep(`Action: Set a valid username and password`, async () => {
+		//await allure.createStep(`Action: Set a valid username and password`, async () => {
 			await LoginActionService.login(loginPage);
-		})();
-		await allure.createStep('The home page is displayed', async () => {
+		//})();
+		//await allure.createStep('The home page is displayed', async () => {
 			await mainPage.waitToBePresent();
-			await because('The logged user is Administrator').expect(await mainPage.getFullUsernameField().getText()).toEqual('Administrator');
-		})();
+			/*await because('The logged user is Administrator').*/expect(await mainPage.getFullUsernameField().getText()).toEqual('Administrator');
+		//})();
 	});
 
 	it('Login with an incorrect password', async () => {
-		await allure.createStep(`Action: Set an invalid username and password`, async () => {
+		//await allure.createStep(`Action: Set an invalid username and password`, async () => {
 			await LoginActionService.login(loginPage, wrongUser.user, wrongUser.password);
-		})();
-		await because('Invalid username or password message is displayed')
-			.expect(await loginPage.getMessagePopup().getTextMessage()).toEqual('Invalid username or password');
+		//})();
+		/*await because('Invalid username or password message is displayed')
+			.*/expect(await loginPage.getMessagePopup().getTextMessage()).toEqual('Invalid username or password');
 	});
 });
