297cdf0446175646635b0d1753ca430e227ec688
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-11-30 23:17:33 +0100

    Used AssertionUtility (instead of Jasmine assertions)

--- e2e-wdio/src/login/tests/login.e2e-spec.ts
+++ e2e-wdio/src/login/tests/login.e2e-spec.ts
@@ -2,7 +2,7 @@ import { LoginPage} from '../page-objects/login.po';
 import { MainPage } from '../../main/page-objects/main.po';
 import { GeneralParameters } from '../../../general-parameters';
 import { LoginActionService } from '../services/login-action.service';
-import { ReportUtility, TestIdentification } from 'systelab-components-wdio-test';
+import { AssertionUtility, ReportUtility, TestIdentification } from 'systelab-components-wdio-test';
 
 
 describe('TC0002_LoginManagement_e2e', () => {
@@ -19,18 +19,18 @@ describe('TC0002_LoginManagement_e2e', () => {
 		mainPage = new MainPage();
 	});
 
-	it('Set a valid username and password', async () =>
+	it('Fill in the login form with a valid username and password', async () =>
 	{
 		await LoginActionService.login(loginPage);
 		await mainPage.waitToBePresent();
 
-		await ReportUtility.addExpectedResult('The logged user is Administrator', async () =>
+		await ReportUtility.addExpectedResult("The logged user is 'Administrator'", async () =>
 		{
-			expect(await mainPage.getFullUsernameField().getText()).toEqual('Administrator');
+			AssertionUtility.expectEqual(await mainPage.getFullUsernameField().getText(), 'Administrator');
 		});
 	});
 
-	it('Set an invalid username and password', async () =>
+	it('Fill in the login form with an invalid username and password', async () =>
 	{
 		const wrongUser = {
 			user: 'noUser',
@@ -38,9 +38,9 @@ describe('TC0002_LoginManagement_e2e', () => {
 		};
 		await LoginActionService.login(loginPage, wrongUser.user, wrongUser.password);
 	
-		await ReportUtility.addExpectedResult('Invalid username or password message is displayed', async () =>
+		await ReportUtility.addExpectedResult("'Invalid username or password' message is displayed", async () =>
 		{
-			expect(await loginPage.getMessagePopup().getTextMessage()).toEqual('Invalid username or password');
+			AssertionUtility.expectEqual(await loginPage.getMessagePopup().getTextMessage(), 'Invalid username or password');
 		});
 	});
 });
