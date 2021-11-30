11c4c7ae2475985786cdf39da6db81fe0b448af4
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2021-11-30 08:03:27 +0100

    Used report utility and test identification

--- e2e-wdio/src/login/tests/login.e2e-spec.ts
+++ e2e-wdio/src/login/tests/login.e2e-spec.ts
@@ -1,42 +1,46 @@
 import { LoginPage} from '../page-objects/login.po';
 import { MainPage } from '../../main/page-objects/main.po';
-//import { because, TestUtil } from 'systelab-components-test/lib/utilities';
 import { GeneralParameters } from '../../../general-parameters';
 import { LoginActionService } from '../services/login-action.service';
+import { ReportUtility, TestIdentification } from 'systelab-components-wdio-test';
 
-//declare const allure: any;
 
 describe('TC0002_LoginManagement_e2e', () => {
 	let loginPage: LoginPage;
 	let mainPage: MainPage;
 
-	const wrongUser = {
-		user: 'noUser',
-		password: 'noPass'
-	};
-
 	beforeEach(async () => {
-		// TestUtil.init('TC0002_LoginManagement_e2e', 'Goal: The purpose of this test case is to verify the login and log out functionalities',
-		// 	GeneralParameters.appVersion, GeneralParameters.USERNAME);
+		TestIdentification.setTmsLink("TC0002_LoginManagement_e2e");
+		TestIdentification.setDescription("Goal: The purpose of this test case is to verify the login and log out functionalities");
+		TestIdentification.setAppVersion(GeneralParameters.appVersion);
+		TestIdentification.captureEnvironment();
+
 		loginPage = new LoginPage();
 		mainPage = new MainPage();
 	});
 
-	it('Login correct', async () => {
-		//await allure.createStep(`Action: Set a valid username and password`, async () => {
-			await LoginActionService.login(loginPage);
-		//})();
-		//await allure.createStep('The home page is displayed', async () => {
-			await mainPage.waitToBePresent();
-			/*await because('The logged user is Administrator').*/expect(await mainPage.getFullUsernameField().getText()).toEqual('Administrator');
-		//})();
+	it('Set a valid username and password', async () =>
+	{
+		await LoginActionService.login(loginPage);
+		await mainPage.waitToBePresent();
+
+		await ReportUtility.addExpectedResult('The logged user is Administrator', async () =>
+		{
+			expect(await mainPage.getFullUsernameField().getText()).toEqual('Administrator');
+		});
 	});
 
-	it('Login with an incorrect password', async () => {
-		//await allure.createStep(`Action: Set an invalid username and password`, async () => {
-			await LoginActionService.login(loginPage, wrongUser.user, wrongUser.password);
-		//})();
-		/*await because('Invalid username or password message is displayed')
-			.*/expect(await loginPage.getMessagePopup().getTextMessage()).toEqual('Invalid username or password');
+	it('Set an invalid username and password', async () =>
+	{
+		const wrongUser = {
+			user: 'noUser',
+			password: 'noPass'
+		};
+		await LoginActionService.login(loginPage, wrongUser.user, wrongUser.password);
+	
+		await ReportUtility.addExpectedResult('Invalid username or password message is displayed', async () =>
+		{
+			expect(await loginPage.getMessagePopup().getTextMessage()).toEqual('Invalid username or password');
+		});
 	});
 });
