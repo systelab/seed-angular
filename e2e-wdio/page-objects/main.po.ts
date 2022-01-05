84fd62a488b62d3ac19230c1e1017916a79dfdac
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2022-01-05 11:06:22 +0100

    Migrated most methods of main page object

--- e2e-wdio/page-objects/main.po.ts
+++ e2e-wdio/page-objects/main.po.ts
@@ -1,6 +1,7 @@
-// import {AllergyDetailDialog} from "./allergy/allergy-detail/allergy-dialog";
+import { BasePage, Button, Grid, Icon, Label, Tabs } from "systelab-components-wdio-test";
+
+import { AllergyDetailDialog } from "./allergy-dialog.po";
 // import {PatientMaintenanceDialog} from "./patient/patient-maintenance";
-import { BasePage, Button, /*Grid, Icon,*/ Label/*, Tabs*/ } from "systelab-components-wdio-test";
 
 
 export class MainPage extends BasePage {
@@ -13,37 +14,37 @@ export class MainPage extends BasePage {
         return new Label(this.byId("username"));
     }
 
-    // 	public getPatientButton(): Button {
-    // 		return new Button(this.current.element(by.tagName("systelab-app-header")).all(by.tagName("button")).get(0));
-    // 	}
+    public getPatientButton(): Button {
+        return new Button(this.byTagName("systelab-app-header").$$("button")[0]);
+    }
 
-    // 	public getConfigIcon(): Icon {
-    // 		return new Icon(this.current.element(by.tagName("systelab-app-sidebar-small")).all(by.tagName("li")).get(3));
-    // 	}
+    public getConfigIcon(): Icon {
+        return new Icon(this.byTagName("systelab-app-sidebar-small").$$("li")[3]);
+    }
 
-    // 	public getAllergyOptionsButton(): Button {
-    // 		return new Button(this.current.element(by.id("AllergyMaintenanceOptionsButton")));
-    // 	}
+    public getAllergyOptionsButton(): Button {
+        return new Button(this.byId("AllergyMaintenanceOptionsButton"));
+    }
 
-    // 	public getAllergyAddButton(): Button {
-    // 		return new Button(this.current.element(by.id("AllergyMaintenanceAddButton")));
-    // 	}
+    public getAllergyAddButton(): Button {
+        return new Button(this.byId("AllergyMaintenanceAddButton"));
+    }
 
-    // 	public getAllergyRefreshButton(): Button {
-    // 		return new Button(this.current.element(by.id("AllergyMaintenanceRefreshButton")));
-    // 	}
+    public getAllergyRefreshButton(): Button {
+        return new Button(this.byId("AllergyMaintenanceRefreshButton"));
+    }
 
-    // 	public getAllergyGrid(): Grid {
-    // 		return new Grid(this.current.element(by.id("AllergyTable")));
-    // 	}
+    public getAllergyGrid(): Grid {
+        return new Grid(this.byId("AllergyTable"));
+    }
 
-    // 	public getConfigTabs(): Tabs {
-    // 		return new Tabs(this.current.element(by.tagName("systelab-tabs")));
-    // 	}
+    public getConfigTabs(): Tabs {
+        return new Tabs(this.byTagName("systelab-tabs"));
+    }
 
-    // 	public getAllergyDetailDialog(): AllergyDetailDialog {
-    // 		return new AllergyDetailDialog(element(by.tagName("allergy-dialog")));
-    // 	}
+    public getAllergyDetailDialog(): AllergyDetailDialog {
+        return new AllergyDetailDialog(this.byTagName("allergy-dialog"));
+    }
 
     // 	public getPatientMaintenanceDialog(): PatientMaintenanceDialog {
     // 		return new PatientMaintenanceDialog(element(by.tagName("patient-maintenance-dialog")));
