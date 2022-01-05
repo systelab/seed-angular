3c7276753e6423e4a9524ce83d31422cc2225281
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2022-01-05 11:42:31 +0100

    Added wrapper for utility to disable CSS animations

---
+++ e2e-wdio/utils/disable-css-animation.ts
@@ -0,0 +1,16 @@
+
+export class CSSAnimationUtility {
+
+    public static async disable(): Promise<void> {
+        // 	browser.executeScript(()=> {
+        // 		var css = '* {' +
+        // 				'animation: none !important;' +
+        // 				'}',
+        // 			head = document.head || document.getElementsByTagName('head')[0],
+        // 			style = document.createElement('style');
+        // 		style.type = 'text/css';
+        // 		style.appendChild(document.createTextNode(css));
+        // 		head.appendChild(style);
+        // 	});
+    }
+}
