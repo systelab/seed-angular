3a3daa13fd339ce33f75d81ad13b2912c3656c03
Joaquim Vila Bou <36737101+joaquimvila@users.noreply.github.com>
2022-01-17 09:09:21 +0100

    Added condition waits to make sure patient creation is not flaky

--- e2e-wdio/services/main-action.service.ts
+++ e2e-wdio/services/main-action.service.ts
@@ -1,18 +1,23 @@
 import { MainPage } from "@e2e-pages";
+import { Allergy, Patient } from "@e2e-model";
 
 
 export class MainActionService {
 
-    public static async createPatient(mainPage: MainPage, patient) {
+    public static async createPatient(mainPage: MainPage, patient: Patient) {
         await mainPage.getPatientButton().click();
+        await mainPage.getPatientMaintenanceDialog().waitToBePresent();
+        const nPatientsBefore = await mainPage.getPatientMaintenanceDialog().getPatientsGrid().getNumberOfRows();
         await mainPage.getPatientMaintenanceDialog().getButtonAdd().click();
         const patientDialog = await mainPage.getPatientMaintenanceDialog().getPatientDialog();
+        await patientDialog.waitToBePresent();
         await patientDialog.set(patient);
         await patientDialog.getButtonSubmit().click();
+        await mainPage.getPatientMaintenanceDialog().waitUntil(async () => (await mainPage.getPatientMaintenanceDialog().getPatientsGrid().getNumberOfRows()) == nPatientsBefore + 1);
         await mainPage.getPatientMaintenanceDialog().close();
     }
 
-    public static async createAllergy(mainPage: MainPage, allergy) {
+    public static async createAllergy(mainPage: MainPage, allergy: Allergy) {
         await mainPage.getConfigIcon().click();
         await mainPage.getAllergyAddButton().click();
         const allergyDialog = await mainPage.getAllergyDetailDialog();
